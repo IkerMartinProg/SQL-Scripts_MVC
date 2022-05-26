121eeedc02cc6f5543fe34592fba7049fb9867c7
IkerMartin <ikermartinprog@gmail.com>
2022-05-26 15:42:22 +0200

    Segunda versión

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -20,3 +20,32 @@ begin
 	else
 	set @Mensaje = 'El correo del usuario ya existe'
 end
+go
+
+create proc sp_EditarUsuario(
+@IdUsuario int,
+@Nombres varchar(100),
+@Apellidos varchar(100),
+@Correo varchar(100),
+@Activo bit,
+@Mensaje varchar(500) output,
+@Resultado bit output
+)
+as
+begin
+	SET @Resultado = 0
+	IF NOT EXISTS (SELECT * FROM USUARIO WHERE Correo = @Correo and IdUsuario != @IdUsuario)
+	begin
+
+		UPDATE top (1) USUARIO SET
+		Nombres = @Nombres,
+		Apellidos = @Apellidos,
+		Correo = @Correo,
+		Activo = @Activo
+		WHERE IdUsuario = @IdUsuario
+
+		SET @Resultado = 1
+	end
+	else
+	set @Mensaje = 'El correo del usuario ya existe'
+end
